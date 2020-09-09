dced826577f39bb3dd540d7d2b65fca23a61f5f3
Chris Herbst <cherbst@gmail.com>
2020-09-09 10:03:55 -0400

    new alg v2

--- testCases/course2/assignment2Dijkstra/dijkstra.rb
+++ testCases/course2/assignment2Dijkstra/dijkstra.rb
@@ -37,36 +37,71 @@ def dijkstra(list)
 
   # graph.write_to_graphic_file('jpg')
 
-# graph.dijkstra_shortest_path(edge_weights, 1, 7)
-# [1, 8, 103, 84, 169, 100, 147, 120, 132, 16, 33, 7]
+  # [1] pry(#<dijkstra>)> graph.dijkstra_shortest_path(edge_weights, 1, 188)
+  # => [1, 156, 93, 24, 192, 59, 6, 60, 188]
 
   targets = [7,37,59,82,99,115,133,165,188,197]
   results = []
 
-  for target in targets
-    queue = [[1, 0]]
+  # for target in targets
+  #   queue = [[1, 0]]
 
-    visited = Set.new
-    result = 2 ** 32
+  #   visited = Set.new
+  #   result = 2 ** 32
+
+  #   while !queue.empty?
+  #     u, weight = queue.shift
+
+  #     if u == target
+  #       result = [result, weight].min
+  #     else
+  #       visited << u
+
+  #       for nei in graph.adjacent_vertices(u)
+  #         next if visited.include?(nei)
+
+  #         queue << [nei, weight + edge_weights[[u, nei]]]
+  #       end
+  #     end
+  #   end
+
+  #   results << result
+  # end
+
+  dist = Hash.new(2**32)
+  dist[1] = 0
+  visited = Set.new
+
+  # while queue.length > 0
+  #   u = queue.shift
+
+  #   next if visited.include?(u)
+  #   visited << u
+
+  #   puts [__LINE__, u].inspect
+
+  #   for nei in graph.adjacent_vertices(u)
+  #     dist[nei] = [dist[nei], dist[u] + edge_weights[[u, nei]]].min
+  #   end
+  # end
+
+  # puts dist.inspect
+  while visited.length < graph.vertices.length
+    queue = [(dist.keys.sort { |a,b| dist[a] <=> dist[b] } - visited.to_a).first]
 
     while !queue.empty?
-      u, weight = queue.shift
+      u = queue.shift
 
-      if u == target
-        result = [result, weight].min
-      else
-        visited << u
+      next if visited.include?(u)
+      visited << u
 
-        for nei in graph.adjacent_vertices(u)
-          next if visited.include?(nei)
-
-          queue << [nei, weight + edge_weights[[u, nei]]]
-        end
+      for nei in graph.adjacent_vertices(u)
+        dist[nei] = [dist[nei], dist[u] + edge_weights[[u, nei]]].min
       end
     end
-
-    results << result
   end
+
+  targets.each { |e| results << dist[e] }
 
   return results
 end
@@ -83,10 +118,9 @@ describe 'dijkstra' do
     examples = [
       'random_1_4',
       'random_2_4',
-      # TODO getting wrong result for this next one
-      # 'random_3_4',
-      # 'random_4_4',
-      # 'random_5_8',
+      'random_3_4',
+      'random_4_4',
+      'random_5_8',
       # 'random_6_8',
       # 'random_7_8',
       # 'random_8_8',
