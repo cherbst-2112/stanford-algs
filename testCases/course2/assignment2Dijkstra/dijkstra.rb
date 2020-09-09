11feef8f4610ee4dbe6a3cb13baef015c0716237
Chris Herbst <cherbst@gmail.com>
2020-09-08 22:12:37 -0400

    Correctly calculates depth for node 7.

---
+++ testCases/course2/assignment2Dijkstra/dijkstra.rb
@@ -0,0 +1,119 @@
+require 'set'
+require 'json'
+require 'rgl/adjacency'
+require 'rgl/dot'
+require 'rgl/path'
+require 'pry'
+require 'rgl/dijkstra'
+
+
+def dijkstra(list)
+  graph = RGL::DirectedAdjacencyGraph.new
+
+  # edge_weights =
+  # {
+  #   ["New York", "Los Angeles"] => 2445,
+  #   ["Los Angeles", "Chicago"] => 2015,
+  #   ["Los Angeles", "Houston"] => 1547,
+  #   ["Chicago", "Houston"] => 939,
+  #   ["Seattle", "Los Angeles"] => 1548
+  # }
+  # edge_weights.each { |(city1, city2), w| graph.add_edge(city1, city2) }
+
+  edge_weights = {}
+
+  list.each do |e|
+    vertices = e.split
+
+    u = vertices.shift.to_i
+
+    while !vertices.empty?
+      v, weight = vertices.shift.split(',').map(&:to_i)
+      edge_weights[[u, v]] = weight
+
+      graph.add_edge(u, v)
+    end
+  end
+
+  # graph.write_to_graphic_file('jpg')
+
+# graph.dijkstra_shortest_path(edge_weights, 1, 7)
+# [1, 8, 103, 84, 169, 100, 147, 120, 132, 16, 33, 7]
+
+  queue = [[1, 0]]
+
+  visited = Set.new
+  result = 2 ** 32
+
+  while !queue.empty?
+    u, weight = queue.shift
+
+    if u == 7
+      result = [result, weight].min
+      next
+    end
+
+    visited << u
+
+    # if v = (graph.adjacent_vertices(u) - visited.to_a).sort { |a,b| edge_weights[[u, a]] <=> edge_weights[[u, b]] }.first
+    #   stack << v
+    #   result += edge_weights[[u, v]]
+    # end
+
+    for nei in graph.adjacent_vertices(u)
+      next if visited.include?(nei)
+
+      queue << [nei, weight + edge_weights[[u, nei]]]
+    end
+  end
+
+  return result
+end
+
+
+require "minitest/autorun"
+
+describe 'dijkstra' do
+  # specify do
+  #   # TODO we get [533586, 1036, 618, 480, 404] like the python, but this is not correct
+  #   assert_equal -1, scc(File.read('SCC.txt').split("\n"))
+  # end
+
+  specify do
+    examples = [
+      'random_1_4',
+      # 'random_2_4',
+      # 'random_3_4',
+      # 'random_4_4',
+      # 'random_5_8',
+      # 'random_6_8',
+      # 'random_7_8',
+      # 'random_8_8',
+      # 'random_9_16',
+      # 'random_10_16',
+      # 'random_11_16',
+      # 'random_12_16',
+      # 'random_13_32',
+      # 'random_14_32',
+      # 'random_15_32',
+      # 'random_16_32',
+      # 'random_17_64',
+      # 'random_18_64',
+      # 'random_19_64',
+      # 'random_20_64',
+      # 'random_21_128',
+      # 'random_22_128',
+      # 'random_23_128',
+      # 'random_24_128',
+      # 'random_25_256',
+      # 'random_26_256',
+      # 'random_27_256',
+      # 'random_28_256'
+    ]
+
+    for e in examples
+      puts "running #{e}..."
+      assert_equal File.read("output_#{e}.txt").split(',').map(&:to_i), dijkstra(File.read("input_#{e}.txt").split("\n"))
+    end
+  end
+end
